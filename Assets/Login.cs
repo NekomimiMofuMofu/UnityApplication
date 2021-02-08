534f6f9de7fe4b4ab212102dca66e602933f1d03
sin_ni <0bl6zqh7li@gmail.com>
2021-02-08 11:36:30 +0900

    固定分でログインできるようにしようとしたけどコンポーネントの取得が分からん

--- Assets/Login.cs
+++ Assets/Login.cs
@@ -1,12 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Login : MonoBehaviour
 {
     void Start()
     {
-        SceneManager.LoadScene("Scenes/MenuScene");
+        string LoginName = "admin";
+        string PassWord = "aaaa";
+
+        Text InputNameText = GameObject.Find("NameInputField/Text").GetComponent<Text>();
+        Text InputPassText = GameObject.Find("PassInputField/Text").GetComponent<Text>();
+
+        string InputName = InputNameText.ToString();
+        string InputPass = InputPassText.ToString();
+        
+        bool NameAuth = LoginName == InputName;
+        bool PassAuth = PassWord == InputPass;
+        
+        Debug.Log("LoginName is " + LoginName);
+        Debug.Log("InputName is " + InputName);
+        Debug.Log("InputNameText is " + InputNameText);
+        Debug.Log("InputPassText is " + InputPassText);
+        Debug.Log("PassWord is " + PassWord);
+        Debug.Log("InputPass is " + InputPass);
+        
+        if (NameAuth && PassAuth)
+        {
+            SceneManager.LoadScene("Scenes/MenuScene");
+        }
     }
 }
